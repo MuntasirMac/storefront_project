a0bf6c88714e9f8c6d7d6fa72269fdee7a847453
MuntasirMac <Muntasir120@gmail.com>
2022-10-28 00:51:08 +0600

    more features added

--- store/views.py
+++ store/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import get_object_or_404
+from store.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
+from store.pagination import DefaultPagination
 from django.db.models.aggregates import Count
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.decorators import action, permission_classes
 from rest_framework.filters import SearchFilter, OrderingFilter
-from .pagination import DefaultPagination
-from .models import Product, Collection, Review, Cart, CartItem
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import status
 from .filters import ProductFilter
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, AddCartItemSerializer,\
-    UpdateCartItemSerializer
-# Create your views here.
+from .models import Cart, CartItem, Collection, Customer, Product, Review
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CollectionSerializer, CustomerSerializer, ProductSerializer, ReviewSerializer, UpdateCartItemSerializer
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -19,84 +21,90 @@ class ProductViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     pagination_class = DefaultPagination
+    permission_classes = [IsAdminOrReadOnly]
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
-    # def get_queryset(self):
-    #     queryset = Product.objects.all()
-    #     collection_id = self.request.query_params.get('collection_id')
-    #     if collection_id is not None:
-    #         queryset = queryset.filter(collection_id=collection_id)
 
-    #     return queryset
-    
     def get_serializer_context(self):
         return {'request': self.request}
-    
+
     def delete(self, request, pk):
-        product = get_object_or_404(Product,pk=pk)
-        if product.orderitem_set.count() > 0:
-            return Response({'Message':'Product cannot be deleted because it is associate with an order item'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product = get_object_or_404(Product, pk=pk)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-# class ProductList(ListCreateAPIView):
-        
-# class ProductDetail(RetrieveUpdateDestroyAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-    
+
+
 class CollectionViewSet(ModelViewSet):
-    queryset = Collection.objects.annotate(products_count=Count('product')).all()
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
     serializer_class = CollectionSerializer
-    
+    permission_classes = [IsAdminOrReadOnly]
+
     def delete(self, request, pk):
-        collection = get_object_or_404(
-        Collection, pk=pk
-    )
+        collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
-            return Response({'Message':'Collection cannot be deleted because it is associate with a product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# class CollectionList(ListCreateAPIView):
-    
-# class CollectionDetail(RetrieveUpdateDestroyAPIView):
-#     queryset = Collection.objects.annotate(
-#             products_count=Count('product')
-#         )
-#     serializer_class = CollectionSerializer
 
-    
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
-    
+
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
-    
+
     def get_serializer_context(self):
-        return {'product_id':self.kwargs.get('product_pk')}
-    
+        return {'product_id': self.kwargs['product_pk']}
+
+
 class CartViewSet(CreateModelMixin,
-                  RetrieveModelMixin, 
-                  DestroyModelMixin, 
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
                   GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
-    
+
+
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
-    
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return AddCartItemSerializer
         elif self.request.method == 'PATCH':
             return UpdateCartItemSerializer
         return CartItemSerializer
-    
+
     def get_serializer_context(self):
-        return {'cart_id':self.kwargs.get('cart_pk')}
-    
+        return {'cart_id': self.kwargs['cart_pk']}
+
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs.get('cart_pk'))\
+        return CartItem.objects \
+            .filter(cart_id=self.kwargs['cart_pk']) \
             .select_related('product')
-            
+
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=True, permission_classes=[ViewCustomerHistoryPermission])
+    def history(self, request, pk):
+        return Response('ok')
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(
+            user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
--- storefront/urls.py
+++ storefront/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 # hello
+
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
 
